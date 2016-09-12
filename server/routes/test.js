b52bae4621cf845ed1d80a8543a0879fe3fcfdc1
Pallandor <roger.sejas@gmail.com>
2016-09-13 09:55:49 +1000

    Fix lint errors

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,14 +2,14 @@
 
 /** API Route Handlers **/
 
-exports.postTest = (req, res, next) => {
+exports.postTest = (req, res) => {
   res.status(200)
     .json({
       didThis: 'test work????',
     });
 };
 
-exports.getTest = (req, res, next) => {
+exports.getTest = (req, res) => {
   res.status(200)
     .json({
       howAboutGet: 'did it also work????',
