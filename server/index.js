a6ff400c9a034539d521ad4693bcdec8c0efe2cf
Pallandor <roger.sejas@gmail.com>
2016-09-13 09:41:06 +1000

    Add dotenv to load environment variables in node for local dev build
    * Also add NPM dev server script

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const winston = require('winston');
 const helmet = require('helmet');
 
-// /** Load environment variables from .env file if dev mode **/
-// if (process.env.NODE_ENV === 'development') {
-//   dotenv.load({ path: '.env' });
-// }
+/** Load environment variables from .env file if dev mode **/
+if (process.env.NODE_ENV === 'development') {
+  dotenv.load({ path: '.env' });
+  console.log(process.env.TEST_KEY);
+}
 
 /** Create Express Server **/
 const app = express();
@@ -18,17 +19,6 @@ const PORT = process.env.PORT || 8080;
 /** Server config **/
 app.use(bodyParser.json());
 app.use(helmet());
-
-// Test up and running!
-app.get('/', (req, res, next)=> {
-  res.status(200).send(req.body);
-  console.log("send something to client!");
-});
-
-app.post('/test', (req, res, next)=> {
-  res.status(200).send(req.body);
-  console.log("send something to client!");
-});
 
 /** Start Server **/
 app.listen(PORT, (err) => {
