70df6bdd8f74aee3c70831422a19a9c66acab5ab
Pallandor <roger.sejas@gmail.com>
2016-09-14 10:55:02 +1000

    Add custom deep object key equality validator to server utils

--- server/controllers/api/tvshow.js
+++ server/controllers/api/tvshow.js
@@ -1,21 +1,11 @@
 'use strict';
 
 const config = require('../../routes/config');
+const util = require('../../util');
 
-const hasEqualKeys = (obj1, obj2) => {
-  const obj1Keys = Object.keys(obj1);
-	const obj2Keys = Object.keys(obj2);
-  if (obj1Keys.length !== obj2Keys.length) return false;
-  return obj1Keys.every(obj1Key => {
-    if (typeof obj1[obj1Key] === 'object' && typeof obj2[obj1Key] === 'object' && obj1[obj1Key] !== null && obj2[obj1Key] !== null) {
-      return hasEqualKeys(obj1[obj1Key], obj2[obj1Key]);
-    }
-    return obj2.hasOwnProperty(obj1Key);
-  });
-};
 
 exports.validateShows = (shows, expectedShowObjShape = config.defaultExpectedShowShape) =>
-  
+
 
 // implement lazy loading? no.
 util.validateShows(shows)
