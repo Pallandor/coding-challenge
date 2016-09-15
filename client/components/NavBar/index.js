57f74d3343a7f84fd33bf68f466077c17b142153
Pallandor <roger.sejas@gmail.com>
2016-09-16 02:29:02 +1000

    Modify NavBar Component, add Links and Tachyon styling

--- client/components/NavBar/index.js
+++ client/components/NavBar/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const NavBar = () => (
-  <div>
-    <ul>
-      <li>NavBar Menu 1</li>
-      <li>NavBar Menu 2</li>
-      <li>NavBar Menu 3</li>
-    </ul>
-  </div>
+  <nav className={styles.nav}>
+    <Link className={styles.leftLink} to={'/'}>Home</Link>
+    <div className={styles.rightContainer}>
+      <Link className={styles.rightLink} to={'/'}>TV Shows</Link>
+      <Link className={styles.rightLink} to={'about'}>About</Link>
+      <a href='https://www.github.com/pallandor/coding-challenge' className={styles.lastRightLink}>Github</a>
+    </div>
+  </nav>
 );
+
+const styles = {
+  nav: 'dt w-100 border-box pa3 ph5-ns',
+  rightContainer: 'dtc v-mid w-75 tr',
+  leftLink: 'dtc v-mid link dim w-25 black b',
+  rightLink: 'link dim dark-gray f6 f5-ns dib mr3 mr4-ns',
+  lastRightLink: 'link dim dark-gray f6 f5-ns dib',
+};
 
 export default NavBar;
