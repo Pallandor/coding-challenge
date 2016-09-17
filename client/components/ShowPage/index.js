382f5eb82f7ee0672fb0e41e3f181fff4f1f26e3
Pallandor <roger.sejas@gmail.com>
2016-09-17 13:58:07 +1000

    Add Tachyons Styling to ShowPage Component

--- client/components/ShowPage/index.js
+++ client/components/ShowPage/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const ShowPage = ({title, tvChannel, description, imageLink}) => (
-    <div>
-      <h3>This is a ShowPage Component</h3>
-      <h3>Title is: {title}</h3>
-      <h3>Tv Channel is {tvChannel}</h3>
-      <h3>Description is  {description}</h3>
-      <h3>ImageLink is: {imageLink}</h3>
+  <article className="cf ph3 ph5-ns pv4">
+    <header className="fn fl-ns w-50-ns pr4-ns">
+      <h1 className="f2 lh-title fw9 mb3 mt0 pt3 bt bw2">{title}</h1>
+      <h2 className="f3 mid-gray lh-title">Channel: {tvChannel}</h2>
+      <p className="f6 ttu tracked gray">Show Description</p>
+      <p className="f5 lh-copy measure mt0-ns">{description}</p>
+    </header>
+    <div className="fn fl-ns w-50-ns">
+      <img src={imageLink} className="w-100 f5 measure" alt="something here!" />
     </div>
-  );
+  </article>
+);
 
 ShowPage.defaultProps = {
   title: 'Title not found',
