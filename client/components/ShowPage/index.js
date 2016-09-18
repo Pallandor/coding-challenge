34784d5aacf0bace931d3513504797c266eecd69
Pallandor <roger.sejas@gmail.com>
2016-09-19 06:52:41 +1000

    Move ShowPage styling to colocated object

--- client/components/ShowPage/index.js
+++ client/components/ShowPage/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
 const ShowPage = ({title, tvChannel, description, imageLink}) => (
-  <article className="cf ph3 ph5-ns pv4">
-    <header className="fn fl-ns w-50-ns pr4-ns">
-      <h1 className="f2 lh-title fw9 mb3 mt0 pt3 bt bw2">{title}</h1>
-      <h2 className="f3 mid-gray lh-title">Channel: {tvChannel}</h2>
-      <p className="f6 ttu tracked gray">Show Description</p>
-      <p className="f5 lh-copy measure mt0-ns">{description}</p>
+  <article className={styles.page}>
+    <header className={styles.header}>
+      <h1 className={styles.mainTitle}>{title}</h1>
+      <h2 className={styles.channelTitle}>Channel: {tvChannel}</h2>
+      <p className={styles.descriptionTitle}>Show Description</p>
+      <p className={styles.description}>{description}</p>
     </header>
-    <div className="fn fl-ns w-50-ns">
-      <img src={imageLink} className="w-100 f5 measure" alt="something here!" />
+    <div className={styles.imgContainer}>
+      <img src={imageLink} className={styles.img} alt={`Photo of ${title}`} />
     </div>
   </article>
 );
+
+const styles = {
+  page: 'cf ph3 ph5-ns pv4',
+  header: 'fn fl-ns w-50-ns pr4-ns',
+  mainTitle: 'f2 lh-title fw9 mb3 mt0 pt3 bt bw2',
+  channelTitle: 'f3 mid-gray lh-title',
+  descriptionTitle: 'f6 ttu tracked gray',
+  description: 'f5 lh-copy measure mt0-ns',
+  imgContainer: 'fn fl-ns w-50-ns',
+  img: 'w-100 f5 measure',
+};
 
 ShowPage.defaultProps = {
   title: 'Title not found',
