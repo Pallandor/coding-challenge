359f38a4eb3ba4d48d2aa57f47a188aefca27db0
Pallandor <roger.sejas@gmail.com>
2016-09-19 07:40:04 +1000

    Fix AboutPage to render content, add Tachyon styles

--- client/components/About/index.js
+++ client/components/About/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Loading from '../Loading';
 
 const About = () => (
-  <div>
-    <h3>This is the About Component</h3>
-    <Loading />
-  </div>
+  <article className={styles.container}>
+    <h1>About RogFlix</h1>
+    <p className={styles.paragraph}>
+      React/Redux under the hood. React-router takes care of rendering the
+      correct component trees, Redux takes care of handling all state. TV Shows
+      page is an example of Node API consumer, rendering JSON as a list of
+      components, so will change dynamically on reload, when mock data store
+      on Node/Express server is updated.
+    </p>
+  </article>
 );
+
+const styles = {
+  container: 'pa3 pa5-ns',
+  paragraph: 'measure lh-copy',
+};
 
 About.propTypes = {
   children: React.PropTypes.node,
