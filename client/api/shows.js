d11237b678a355d07e85a5231235142936fe958b
Pallandor <roger.sejas@gmail.com>
2016-09-24 02:21:52 +1000

    Fix client-side API modules to work with new application endpoints

--- client/api/shows.js
+++ client/api/shows.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 import { BASE_API_URL } from '../constants';
 
 export const fetchShows = () =>
-  axios.get(`${BASE_API_URL}/tvshow`)
+  axios.get(`${BASE_API_URL}/shows`)
     .then(response => response.data.payload);
--- client/constants/index.js
+++ client/constants/index.js
@@ -1,8 +1,9 @@
+/** Determine if running in development mode **/
 const devmode = process.env.NODE_ENV !== 'production';
 
 /** API Constants **/
 export const BASE_URL = 'https://love-your-tech-stack.herokuapp.com';
-export const BASE_API_URL = devmode ? 'http://localhost:8080/api' : `${BASE_URL}/api`;
+export const BASE_API_URL = devmode ? 'http://localhost:8080' : `${BASE_URL}`;
 
 /** Action Constants **/
 export const FETCH_SHOWS_PENDING = 'App/FETCH_SHOWS_PENDING';
