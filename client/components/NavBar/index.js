af323daf36d8153d6840586ec5cc42e813aae799
Pallandor <roger.sejas@gmail.com>
2016-09-25 23:31:23 +1000

    Fix React-Router/Express static serve bug to allow for refresh on About Component page

--- client/components/NavBar/index.js
+++ client/components/NavBar/index.js
@@ -8,8 +8,8 @@ const NavBar = () => (
     </Link>
     <div className={styles.rightContainer}>
       <Link className={styles.rightLink} to={'/'}>TV Shows</Link>
-      <Link className={styles.rightLink} to={'about'}>About</Link>
-      <a href="https://www.github.com/pallandor/coding-challenge" className={styles.lastRightLink}>Github</a>
+      <Link className={styles.rightLink} to={'/about'}>About</Link>
+      <a href="https://www.github.com/pallandor/coding-challenge" target="_blank" className={styles.lastRightLink}>Github</a>
     </div>
   </nav>
 );
