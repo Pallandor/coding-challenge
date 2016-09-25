6ed88982df7ad23755dc523b7e060a7fe1715862
Pallandor <roger.sejas@gmail.com>
2016-09-25 17:16:07 +1000

    Modify Gulp Test Task to run standalone and gulp-task-friendly processes

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('lint', () => {
           Keep 'build-client' task dependency!
           **/
 gulp.task('test-server', ['build-client'], () => {
-  return gulp.src('./test/index.js', {read: false})
+  return gulp.src('./test/index.js')
     .pipe(plumber())
     .pipe(wait(1500))
     .pipe(mocha())
@@ -46,7 +46,18 @@ gulp.task('test-server', ['build-client'], () => {
     });
 });
 
-gulp.task('nodemon', cb => {
+gulp.task('dev-test-server', ['build-client'], () => {
+  return gulp.src('./test/index.js')
+    .pipe(plumber())
+    .pipe(wait(1500))
+    .pipe(mocha())
+    .once('error', () => {
+      process.exit(1);
+    });
+});
+
+/** Wait for tests and client build before starting dev server! **/
+gulp.task('nodemon', ['dev-test-server', 'build-client'], cb => {
   let started = false;
   return nodemon({
     script: './server',
@@ -71,13 +82,14 @@ gulp.task('nodemon', cb => {
 
 /** Composed Gulp Tasks **/
 gulp.task('test', ['lint', 'test-server']);
+gulp.task('dev-test', ['lint', 'dev-test-server']);
 gulp.task('client', ['clean-client', 'build-client']);
 
 /** Default Task (Client build) **/
 gulp.task('default', ['client']);
 
 /** Development Gulp Tasks **/
-gulp.task('dev', ['client', 'test', 'nodemon'], () => {
+gulp.task('dev', ['client', 'dev-test', 'nodemon'], () => {
   gulp.watch('./server/**/*.js', {debounceDelay: 500}, ['test']);
   gulp.watch('./client/**/*.js', {debounceDelay: 500}, ['lint', 'client']);
   gulp.watch('./test/**/*.js', {debounceDelay: 500}, ['test']);
